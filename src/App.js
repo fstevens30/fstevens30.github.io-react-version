05724fcb8f857ed3aa96a43eb2b08983d8c4418c
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-07 11:29:21 +1300

    Setup imports correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css'; 
+import './App.scss';
+import 'semantic-ui-css/semantic.min.css';
+import Nav from './components/Nav/Nav.jsx';
+import Hero from './components/Hero/Hero.jsx';
+import About from './components/About/About.jsx';
+import TechSkills from './components/TechSkills/TechSkills.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Projects from './components/Projects/Projects.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
 
 export default function App () {
   return (
     <main>
       <Nav/>
-      <Header/>
+      <Hero/>
       <About/>
       <TechSkills/>
       <Contact/>

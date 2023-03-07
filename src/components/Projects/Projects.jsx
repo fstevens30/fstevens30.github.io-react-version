fca003848fca4f0a7e7c499e1e9c77da5c7a4ed5
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-07 16:18:18 +1300

    Continued working on components

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Header, Card, Icon, Button, ButtonContent, Container } from "semantic-ui-react";
+import { Header, Card, Icon, Button, ButtonContent, Container, Image } from "semantic-ui-react";
+import "./Projects.scss";
 
 export default function Projects() {
     return (
@@ -9,29 +10,36 @@ export default function Projects() {
                 Below is some of my work that I am proud to show off, from small personal projects to larger group projects through my studies. Click the view more to explore all of my repositories on GitHub.
             </p>
             <Card.Group>
-                <Card>
+
+                <Card as={Button} onClick={() => window.open("https://github.com/fstevens30/ToDo-List-React")} >
                     <Card.Content>
-                        <Card.Header>Project 1</Card.Header>
+                        <Image size="medium" src={require('../../assets/ToDo-React.png')} />
+                        <Card.Header>To-Do List React Native</Card.Header>
                         <Card.Description>
                             A simple To-Do list app made with React Native.
                         </Card.Description>
                     </Card.Content>
                 </Card>
+
                 <Card>
-                    <Card.Content>
-                        <Card.Header>Project 2</Card.Header>
+                    <Card.Content as={Button} onClick={() => window.open("https://fstevens30.github.io/vegan-stats-webapp/")}>
+                        <Image size="medium" src={require('../../assets/Vegan-Stats.png')} />
+                        <Card.Header>Vegan Stats</Card.Header>
                         <Card.Description>
-                            A simple To-Do list app made with React Native.
+                            A simple web app that displays statistics about the vegan diet.
                         </Card.Description>
                     </Card.Content>
                 </Card>
+
             </Card.Group>
-        <Button animated="fade" onClick={() => window.open("https://github.com/fstevens30?tab=repositories")} size="massive" primary>
+
+        <Button animated="fade" onClick={() => window.open("https://github.com/fstevens30?tab=repositories")} size="massive" primary style={{marginTop: "1em"}}>
             <ButtonContent visible>View More</ButtonContent>
             <ButtonContent hidden>
                 <Icon name='arrow right' />
             </ButtonContent>
         </Button>
+
         </Container>
                     
     );

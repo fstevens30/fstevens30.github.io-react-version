fce85bfde637b72905989095e6c5071346919656
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-07 15:29:31 +1300

    Updated projects with some placeholders

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Card, Icon, Button, ButtonContent, Container } from "semantic-ui-react";
 
 export default function Projects() {
     return (
-        <Header>Projects</Header>
+        <Container className="projects-container">
+            <Header>Projects</Header>
+            <p>
+                Below is some of my work that I am proud to show off, from small personal projects to larger group projects through my studies. Click the view more to explore all of my repositories on GitHub.
+            </p>
+            <Card.Group>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>Project 1</Card.Header>
+                        <Card.Description>
+                            A simple To-Do list app made with React Native.
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>Project 2</Card.Header>
+                        <Card.Description>
+                            A simple To-Do list app made with React Native.
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+            </Card.Group>
+        <Button animated="fade" onClick={() => window.open("https://github.com/fstevens30?tab=repositories")} size="massive" primary>
+            <ButtonContent visible>View More</ButtonContent>
+            <ButtonContent hidden>
+                <Icon name='arrow right' />
+            </ButtonContent>
+        </Button>
+        </Container>
+                    
     );
     }

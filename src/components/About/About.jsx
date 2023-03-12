25d1cf2265d6969f6fe6826fa0fd63ae5e18e0f3
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-03-12 16:50:35 +1300

    CHanged words around

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,12 @@ import "./About.scss";
 export default function About() {
     return (
         <Container className="about-container">
-        <Header>About Me</Header>
+        <Header className="about-header">About Me</Header>
         <p>
-        I am a student at Ara Institute of Canterbury, working towards my Bachelor of Information and Communication Technology. I am interested in the Apple ecosystem and the JavaScript programming language, and hope to use my skills to create full-stack cross-platform applications using React Native. My hobbies include basketball, motorsport and videogames.
+        As a Bachelor of Information and Communication Technology student at Ara Institute of Canterbury, I have a keen interest in the Apple ecosystem and the JavaScript programming language. My goal is to apply my knowledge to develop cross-platform, full-stack applications using React Native. Outside of my studies, I enjoy playing basketball, following motorsport events, and playing video games.
         </p>
         <p>
-        I am active on most social media platforms, the best way to contact me is via Messenger or Email. You can learn more about me by downloading my CV below!
+        I am available on various social media platforms, but for the most prompt response, please reach out to me via Messenger or Email. Additionally, you can find more information about me by downloading my CV below.
         </p> 
         {/* Button that downloads my cv stored in assets folder */}
         <Button primary animated="fade" onClick={() => window.open(require('../../assets/Flynn-Stevens-CV.pdf'))} size="massive">
@@ -20,14 +20,13 @@ export default function About() {
             </ButtonContent>
         </Button>
         {/* Button that takes user to the contact element on the page */}
-        <Button animated="fade" onClick={() => window.scrollTo(0, document.body.scrollHeight)} size="massive" color="()">
+        <Button animated="fade" onClick={() => window.scrollTo(0, document.body.scrollHeight)} size="massive">
             <ButtonContent visible>Contact Me</ButtonContent>
             <ButtonContent hidden>
                 <Icon name='arrow down' />
             </ButtonContent>
         </Button>
         </Container>
-
     )
 }
     
